ca4e32e6782175c8b487bf2d28a750458c4c4cd4
Asif Nassar <klmktu.asif@gmail.com>
2025-02-01 22:42:10 +0530

    revert chakra ui

--- chip-in-frontend/src/main.tsx
+++ chip-in-frontend/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import ChipInApp from './ChipInApp.tsx'
-import { Provider } from '@/components/ui/provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <ChipInApp />
-    </Provider>
+    <ChipInApp />
   </StrictMode>,
 )
